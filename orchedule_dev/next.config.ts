c7fe853aa503bc51f23a51bf188010ea18703981
zzemoo <chaerim.dev@gmail.com>
2025-04-06 10:34:40 +0900

    fix : 콘솔 버그 수정

--- orchedule_dev/next.config.ts
+++ orchedule_dev/next.config.ts
@@ -6,11 +6,12 @@ const withPWA = withPWAInit({
   buildExcludes: [/\.next[\\/]trace/],
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
 };
 
 export default withPWA(nextConfig);
