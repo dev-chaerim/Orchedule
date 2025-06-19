e0a3263688feac6bbde6bc5ddc9ea37578fa1c1c
zzemoo <chaerim.dev@gmail.com>
2025-06-19 11:49:15 +0900

    feat: head.tsx 추가

---
+++ orchedule_dev/app/head.tsx
@@ -0,0 +1,12 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, viewport-fit=cover"
+      />
+      <title>Orchedule</title>
+    </>
+  );
+}
