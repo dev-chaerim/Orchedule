6ea48241eae5a3d164a9603cd90b786c3f8033af
zzemoo <chaerim.dev@gmail.com>
2025-06-23 10:46:59 +0900

    feat: 검색엔진 크롤링 차단을 위한 meta 태그 추가

--- orchedule_dev/app/head.tsx
+++ orchedule_dev/app/head.tsx
@@ -1,4 +1,5 @@
 // app/head.tsx
+
 export default function Head() {
   return (
     <>
@@ -6,6 +7,8 @@ export default function Head() {
         name="viewport"
         content="width=device-width, initial-scale=1, viewport-fit=cover"
       />
+      <meta name="robots" content="noindex, nofollow" />{" "}
+      {/* ✅ 검색엔진 차단 */}
       <title>Orchedule</title>
     </>
   );
