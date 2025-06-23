47c69881ea46341f92ddfdf2f1bea5aa6d47b4f3
zzemoo <chaerim.dev@gmail.com>
2025-06-23 11:39:26 +0900

    feat: 파비콘 설정 추가

--- orchedule_dev/app/head.tsx
+++ orchedule_dev/app/head.tsx
@@ -9,6 +9,13 @@ export default function Head() {
       />
       <meta name="robots" content="noindex, nofollow" />{" "}
       {/* ✅ 검색엔진 차단 */}
+      <meta name="theme-color" content="#ffffff" /> {/* ✅ 상태바 색상 설정 */}
+      <meta name="apple-mobile-web-app-title" content="Orchedule" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/apple-icon.png" />
+      <link rel="icon" href="/favicon.ico" />
       <title>Orchedule</title>
     </>
   );
