24744e0b45fc542c86f24a7b9fd1699f41634c1d
zzemoo <chaerim.dev@gmail.com>
2025-06-23 14:49:11 +0900

    feat: api 데이터 캐싱 설정

--- orchedule_dev/next.config.js
+++ orchedule_dev/next.config.js
@@ -1,15 +1,48 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
+const runtimeCaching = [
+  {
+    // 시즌, 공지, 악보체크 API 캐싱
+    urlPattern: /^\/api\/(seasons|notices|score-checks|season-scores).*$/,
+    handler: "NetworkFirst",
+    options: {
+      cacheName: "read-api-cache",
+      expiration: {
+        maxEntries: 30,
+        maxAgeSeconds: 300, // 5분
+      },
+      cacheableResponse: {
+        statuses: [0, 200],
+      },
+    },
+  },
+  {
+    // Google Fonts 캐싱
+    urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+    handler: "CacheFirst",
+    options: {
+      cacheName: "google-fonts",
+      expiration: {
+        maxEntries: 4,
+        maxAgeSeconds: 365 * 24 * 60 * 60, // 1년
+      },
+    },
+  },
+];
+
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
+  runtimeCaching, // ✅ 추가된 부분
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
-//   swcMinify: true,
   images: {
-    domains: ["res.cloudinary.com", "orchestra-schedule-pdf.s3.us-east-2.amazonaws.com"],
+    domains: [
+      "res.cloudinary.com",
+      "orchestra-schedule-pdf.s3.us-east-2.amazonaws.com",
+    ],
   },
 });
